4e85e615516b7fb3662e8c18b158ab30c4e65d5c
zhoujianping <zhoujianping@52tt.com>
2021-08-23 19:01:02 +0800

    learn Combine

--- Neves/TestVC/Base/PropertyWrapper/PropertyWrapperTestVC.swift
+++ Neves/TestVC/Base/PropertyWrapper/PropertyWrapperTestVC.swift
@@ -4,10 +4,11 @@
 //
 //  Created by aa on 2021/7/9.
 //
+//  属性包装器的使用：https://www.jianshu.com/p/8a019631b4db
 
 import Foundation
-import Combine
 
+/// ==================================================================================================
 /// 先告诉编译器 下面这个UserDefault是一个属性包裹器
 @propertyWrapper struct UserDefault<T> {
     /// 这里的属性key 和 defaultValue 还有init方法都是实际业务中的业务代码
@@ -39,8 +40,8 @@ struct UserDefaultsConfig {
     
     @UserDefault("name", defaultValue: "sb") static var name: String
 }
+/// ==================================================================================================
 
-// 属性包装器的使用：https://www.jianshu.com/p/8a019631b4db
 @propertyWrapper struct ObserveChangeValue<T: Equatable> {
     
     private var value: T
@@ -105,27 +106,9 @@ class PropertyWrapperTestVC: TestBaseViewController {
         
         JPrint("taName", taName)
         JPrint("$taName", $taName)
-        
-        // Swift的订阅
-//        let publisher = view.publisher(for: \.backgroundColor, options: .new)
-//        let canceler = publisher.sink {
-//            guard let color = $0 else { return }
-//            JPrint("color \(color)")
-//        }
-//        Asyncs.mainDelay(10) {
-//            JPrint("取消订阅")
-//            canceler.cancel()
-//        }
-        
-//        let tableView = UITableView(frame: PortraitScreenBounds, style: .grouped)
-//        tableView.jp.contentInsetAdjustmentNever()
-//        tableView.dataSource = self
-//        tableView.delegate = self
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-//        view.backgroundColor = .randomColor
-        
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
         nickname = "shuaige \(formatter.string(from: Date()))"
@@ -140,37 +123,6 @@ class PropertyWrapperTestVC: TestBaseViewController {
         JPrint("taName", taName)
         JPrint("-------------")
     }
-}
-
-//extension PropertyWrapperTestVC: UITableViewDataSource, UITableViewDelegate {
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        1
-//    }
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        <#code#>
-//    }
-//}
-
-
-struct Update: Identifiable {
-    var id = UUID()
-    var image: String
-    var title: String
-    var text: String
-    var date: String
-}
-
-let updateData = [
-    Update(image: "Card1", title: "SwiftUI Advanced", text: "Take your SwiftUI app to the App Store with advanced techniques like API data, packages and CMS.", date: "JAN 1"),
-    Update(image: "Card2", title: "Webflow", text: "Design and animate a high converting landing page with advanced interactions, payments and CMS", date: "OCT 17"),
-    Update(image: "Card3", title: "ProtoPie", text: "Quickly prototype advanced animations and interactions for mobile and Web.", date: "AUG 27"),
-    Update(image: "Card4", title: "SwiftUI", text: "Learn how to code custom UIs, animations, gestures and components in Xcode 11", date: "JUNE 26"),
-    Update(image: "Card5", title: "Framer Playground", text: "Create powerful animations and interactions with the Framer X code editor", date: "JUN 11")
-]
-
-class UpdateStore: ObservableObject {
-    @Published var updates: [Update] = updateData
 }
 
 //@propertyWrapper struct Store<[Update]> {
