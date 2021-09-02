bc54dd95fe63e941d5326b7471bba350078daacf
zhoujianping <zhoujianping@52tt.com>
2021-09-02 19:55:15 +0800

    update

---
+++ Neves/TestVC/View&Layer/RelationshipPlanet/PeopleTurntableView.swift
@@ -0,0 +1,70 @@
+//
+//  PeopleTurntableView.swift
+//  Neves
+//
+//  Created by aa on 2021/9/2.
+//
+
+class PeopleTurntableView: UIScrollView {
+    static let radian180 = CGFloat.pi
+    static let radian360 = CGFloat.pi * 2
+    static let radian90 = CGFloat.pi / 2.0
+    static let radian30 = CGFloat.pi / 6.0
+    static let radian105 = radian30 * 3.5
+    static let minContentH = RelationshipPlanet.planetH // 一屏显示半圈，也就是6个
+    static let totalOffsetY = minContentH * 2 // 用于计算转动进度的偏移量（一圈12个，一屏6个，滚一屏高度就是50%进度，所以两屏高度就是刚好能滚一圈的最佳偏移量）
+    static let oneRoundItemCount: Int = 12 // 一圈12个，一屏显示半圈，也就是6个
+    static let halfRoundItemCount: Int = oneRoundItemCount / 2 // 半圈6个
+    static let singleItemRadian: CGFloat = radian360 / CGFloat(oneRoundItemCount) // 360 / 12 = 30
+    static let singleItemOffsetY: CGFloat = minContentH / CGFloat(halfRoundItemCount) // 一屏显示6个
+    
+    lazy var peoples: [PeopleView] = (0 ..< RelationshipPlanet.singlePlanetMaxPeopleCount).map { PeopleView(tag: $0) }
+    var maxOffsetY: CGFloat = 0
+    
+    init() {
+        super.init(frame: [0, 0, RelationshipPlanet.planetW, RelationshipPlanet.planetH])
+        jp.contentInsetAdjustmentNever()
+        delegate = self
+        alwaysBounceVertical = true
+        showsVerticalScrollIndicator = false
+        clipsToBounds = false
+        
+        peoples.forEach { addSubview($0) }
+        
+        let singleItemOffsetY = Self.singleItemOffsetY
+        
+        let minContentH = Self.minContentH
+        var contentH = singleItemOffsetY * CGFloat(peoples.count)
+        if contentH < minContentH { contentH = minContentH }
+        contentSize = [0, contentH]
+        
+        let topInset = singleItemOffsetY // 需要空一格，否则第一个就在中心位置
+        contentInset = .init(top: topInset, left: 0, bottom: 0, right: 0)
+        contentOffset = [0, -topInset]
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension PeopleTurntableView: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let progress = offsetY / Self.totalOffsetY
+        JPrint("offsetY:", offsetY)
+        JPrint("scale:", scale)
+//        JPrint("onePeopleOffsetY:", onePeopleOffsetY)
+//        JPrint("onePeopleRadian:", onePeopleRadian)
+        JPrint("----------------------")
+        
+        peoples.forEach { $0.updateLayout(offsetY: offsetY, progress: progress) }
+    }
+}
+
+extension PeopleTurntableView {
+    func update(scale: CGFloat, alpha: CGFloat) {
+        self.transform = .init(scaleX: scale, y: scale)
+        self.alpha = alpha
+    }
+}
--- Neves/TestVC/View&Layer/RelationshipPlanet/PeopleView.swift
+++ Neves/TestVC/View&Layer/RelationshipPlanet/PeopleView.swift
@@ -6,132 +6,144 @@
 //
 
 class PeopleView: UIView {
+    let minHideOffsetY: CGFloat
+    let minShowOffsetY: CGFloat
     
-    let line = UIView()
+    let nameH: CGFloat = 16.5.px
+    let relH: CGFloat = 16.5.px
+    let relLevelIconWH: CGFloat = 11.3.px
+    let relNumIconWH: CGFloat = 13.px
+    lazy var infoSize: CGSize = [100.px, nameH + 4.px + relH]
     
+    let lineBgView = UIView()
     let userIcon = UIImageView(image: UIImage(named: "jp_icon"))
-    
     let infoView = UIView()
+    
     let nameLabel = UILabel()
-    let levelIcon = UIImageView(image: UIImage(named: "gxq_lv5"))
+    let relLevelIcon = UIImageView(image: UIImage(named: "gxq_lv5"))
     let relBgView = GradientView()
     let relLabel = UILabel()
     let relNumIcon = UIImageView(image: UIImage(named: "gxq_icon_qmz"))
     let relNumLabel = UILabel()
     
     init(tag: Int) {
-        super.init(frame: [0, 0, peopleW, peopleH])
+        let singleItemOffsetY = PeopleTurntableView.singleItemOffsetY
+        self.minHideOffsetY = CGFloat(tag) * singleItemOffsetY - singleItemOffsetY
+        self.minShowOffsetY = CGFloat(tag) * singleItemOffsetY
+        
+        super.init(frame: [0, 0, RelationshipPlanet.peopleW, RelationshipPlanet.peopleH])
         self.tag = tag
-//        backgroundColor = .randomColor
-        layer.anchorPoint = [CGFloat(15.px / peopleW), 0.5]
         
-        line.layer.anchorPoint = [1, 0.5]
-        line.frame = [CGFloat(-20 - 5).px, HalfDiffValue(peopleH, 1.px), CGFloat(20 + 5 + 15).px, 1.px]
-        addSubview(line)
+        let peopleIconWH = RelationshipPlanet.peopleIconWH
+        layer.anchorPoint = [peopleIconWH * 0.5 / frame.width, 0.5]
         
-        let gLayer = CAGradientLayer()
-        gLayer.frame = [0, 0, 20.px, 1.px]
-        gLayer.startPoint = [0, 0]
-        gLayer.endPoint = [1, 0]
-        gLayer.colors = [UIColor.clear.cgColor, UIColor(white: 1, alpha: 1).cgColor, UIColor.clear.cgColor]
-        gLayer.opacity = 0.5
-        line.layer.addSublayer(gLayer)
+        let lineSize: CGSize = [20.px, 1.px]
+        let lineSpace: CGFloat = 5.px
+        lineBgView.layer.anchorPoint = [1, 0.5]
+        lineBgView.frame = [-lineSize.width - lineSpace, HalfDiffValue(frame.height, lineSize.height), lineSize.width + lineSpace + peopleIconWH * 0.5, lineSize.height]
+        addSubview(lineBgView)
         
-        userIcon.frame = [0, 0, peopleH, peopleH]
+        let line = CAGradientLayer()
+        line.frame = .init(origin: .zero, size: lineSize)
+        line.startPoint = [0, 0.5]
+        line.endPoint = [1, 0.5]
+        line.colors = [UIColor.clear.cgColor, UIColor(white: 1, alpha: 1).cgColor, UIColor.clear.cgColor]
+        line.opacity = 0.5
+        lineBgView.layer.addSublayer(line)
+        
+        userIcon.frame = [0, 0, peopleIconWH, peopleIconWH]
         userIcon.contentMode = .scaleAspectFill
         addSubview(userIcon)
         
-        infoView.frame = [CGFloat(30 + 10).px, -3.5.px, 100.px, 37.px]
+        infoView.frame = [userIcon.frame.maxX + 10.px,HalfDiffValue(frame.height, infoSize.height), infoSize.width, infoSize.height]
+        infoView.clipsToBounds = false
         addSubview(infoView)
         
-        nameLabel.frame = [0, 0, 100.px, 16.5.px]
+        nameLabel.frame = [0, 0, infoSize.width * 2, nameH]
         nameLabel.font = .systemFont(ofSize: 12.px)
         nameLabel.textColor = .init(white: 1, alpha: 0.8)
         nameLabel.text = "健了个平 - \(tag)"
         infoView.addSubview(nameLabel)
         
-        let y: CGFloat = nameLabel.maxY + 4.px
-        levelIcon.frame = [1.px, y + HalfDiffValue(16.5.px, 11.3.px), 11.3.px, 11.3.px]
-        infoView.addSubview(levelIcon)
+        let y: CGFloat = infoSize.height - relH
+        relLevelIcon.frame = [1.px, y + HalfDiffValue(relH, relLevelIconWH), relLevelIconWH, relLevelIconWH]
+        infoView.addSubview(relLevelIcon)
         
         relLabel.font = .systemFont(ofSize: 12.px, weight: .medium)
         relLabel.text = "宝贝"
         relLabel.sizeToFit()
-        relLabel.height = 16.5.px
+        relLabel.frame.size.height = relH
         
         relBgView
-            .startPoint(0, 0)
-            .endPoint(1, 0)
+            .startPoint(0, 0.5)
+            .endPoint(1, 0.5)
             .colors(.rgb(117, 138, 255), .rgb(116, 211, 255))
         relBgView.addSubview(relLabel)
         relBgView.mask = relLabel
-        relBgView.frame = [levelIcon.maxX + 5.px, y, relLabel.width, relLabel.height]
+        relBgView.frame = [relLevelIcon.frame.maxX + 5.px, y, relLabel.frame.width, relLabel.frame.height]
         infoView.addSubview(relBgView)
         
-        relNumIcon.frame = [relBgView.maxX + 10.px, y + HalfDiffValue(16.5.px, 13.px), 13.px, 13.px]
+        relNumIcon.frame = [relBgView.maxX + 10.px, y + HalfDiffValue(relH, relNumIconWH), relNumIconWH, relNumIconWH]
         infoView.addSubview(relNumIcon)
         
         relNumLabel.font = .systemFont(ofSize: 12.px, weight: .medium)
         relNumLabel.text = "9999"
         relNumLabel.textColor = .init(white: 1, alpha: 0.5)
         relNumLabel.sizeToFit()
-        relNumLabel.frame = [relNumIcon.maxX + 4.px, y, relNumLabel.width, 16.5.px]
+        relNumLabel.frame = [relNumIcon.maxX + 4.px, y, relNumLabel.frame.width, relH]
         infoView.addSubview(relNumLabel)
-        
-        minShowOffsetY = CGFloat(tag) * onePeopleOffsetY - topInset
-        maxShowOffsetY = CGFloat(tag) * onePeopleOffsetY
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    var minShowOffsetY: CGFloat = 0
-    var maxShowOffsetY: CGFloat = 0
-    
-    func updateAlpha(_ offsetY: CGFloat) {
-        var alpha: CGFloat = 1
+    func updateLayout(offsetY: CGFloat, progress: CGFloat) {
+        let circlePoint = RelationshipPlanet.planetCirclePoint
+        let radius = RelationshipPlanet.planetRadius
+        let radian90 = PeopleTurntableView.radian90
+        let radian105 = PeopleTurntableView.radian105
+        let radian360 = PeopleTurntableView.radian360
+        let singleItemRadian = PeopleTurntableView.singleItemRadian
+        let singleItemOffsetY = PeopleTurntableView.singleItemOffsetY
+        let halfRoundOffsetY = PeopleTurntableView.minContentH // 转半圈所需偏移量
         
-        if offsetY > minShowOffsetY {
-            alpha = 1 - (offsetY - minShowOffsetY) / onePeopleOffsetY
+        // 弧度
+        var radian: CGFloat = singleItemRadian * CGFloat(tag) - radian90
+        radian -= progress * radian360
+        
+        let centerX: CGFloat = circlePoint.x + radius * cos(radian)
+        let centerY: CGFloat = circlePoint.y + radius * sin(radian)
+        center = [centerX, offsetY + centerY]
+        
+        // 线的角度和显示、头像的显示
+        lineBgView.transform = .init(rotationAngle: radian)
+        // 30 * 3.5 = 105
+        if radian > radian105 || radian < -radian105 {
+            lineBgView.isHidden = true
+            userIcon.isHidden = true
         } else {
-            let oy = offsetY + 5 * onePeopleOffsetY + topInset
-            if oy > maxShowOffsetY {
-                alpha = (oy - maxShowOffsetY) / onePeopleOffsetY
+            lineBgView.isHidden = false
+            userIcon.isHidden = false
+        }
+
+        // 信息的透明度
+        var infoAlpha: CGFloat = 1
+        if offsetY > minHideOffsetY {
+            infoAlpha = 1 - (offsetY - minHideOffsetY) / singleItemOffsetY
+        } else {
+            let oy = offsetY + halfRoundOffsetY
+            if oy > minShowOffsetY {
+                infoAlpha = (oy - minShowOffsetY) / singleItemOffsetY
             } else {
-                alpha = 0
+                infoAlpha = 0
             }
         }
-        
-        if alpha < 0 {
-            alpha = 0
-        } else if alpha > 1 {
-            alpha = 1
+        if infoAlpha < 0 {
+            infoAlpha = 0
+        } else if infoAlpha > 1 {
+            infoAlpha = 1
         }
-        infoView.alpha = alpha
-    }
-    
-    private var _radian: CGFloat = 0
-    var currentRradian: CGFloat {
-        set {
-            guard _radian != newValue else { return }
-            _radian = newValue
-            
-            line.transform = .init(rotationAngle: newValue)
-            
-//            if tag == 6 {
-//                JPrint("多少度！！", JPRadian2Angle(newValue))
-//            }
-            
-            // 30 * 3.5 = 105
-            if newValue > radian30 * 3.5 || newValue < -(radian30 * 3.5) {
-                line.isHidden = true
-                userIcon.isHidden = true
-            } else {
-                line.isHidden = false
-                userIcon.isHidden = false
-            }
-        }
-        get { _radian }
+        infoView.alpha = infoAlpha
     }
 }
