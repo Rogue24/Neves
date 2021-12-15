4933005e1779adb17f2cba15949733aaa9846973
zhoujianping24@hotmail.com <zhoujianping24@hotmail.com>
2021-12-15 13:46:19 +0800

    update JPrint

--- Neves/TestVC/FloatWindow/FloatContainer.swift
+++ Neves/TestVC/FloatWindow/FloatContainer.swift
@@ -10,8 +10,9 @@ class FloatContainer: UIView {
     // MARK: 拦截点击
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         guard !isHidden, subviews.count > 0 else { return nil }
-        for subview in subviews where !subview.isHidden && subview.alpha > 0.01 && subview.frame.contains(point) {
-            return subview
+        for subview in subviews.reversed() where !subview.isHidden && subview.alpha > 0.01 && subview.frame.contains(point) {
+            let subPoint = convert(point, to: subview)
+            return subview.hitTest(subPoint, with: event)
         }
         return nil
     }
