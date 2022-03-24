1836626f02d396b79aa3a66d7a104ae4ae4b8efd
zhoujianping <zhoujianping@52tt.com>
2022-03-24 11:33:19 +0800

    修正

--- Neves/TestVC/GCD/GCDAttributesViewController.swift
+++ Neves/TestVC/GCD/GCDAttributesViewController.swift
@@ -12,8 +12,9 @@ class GCDAttributesViewController: TestBaseViewController {
     /// 默认`attributes`啥都不加就是【同步】队列
     /// 往`attributes`加上`.concurrent`就是【异步】队列
     /// 往`attributes`加上`.initiallyInactive`就是个需要【手动启动执行】的队列
-    ///   - 可以调用多次执行，但实际之后执行一次；
-    ///   - 一定要保证执行了！否则当持有该队列的对象销毁时会崩溃！
+    ///   - 可以重复调用`activate`多次执行，但已经执行过的任务只会执行一次，不会重复执行；
+    ///   - 添加新的任务可以再次调用`activate`执行；
+    ///   - 一定要确保队列已经执行了！否则当持有该队列的对象销毁时会崩溃！
     
     // 同步+手动启动
 //    let anotherQueue = DispatchQueue(label: "com.appcoda.anotherQueue", qos: .userInitiated, attributes: .initiallyInactive)
@@ -21,32 +22,29 @@ class GCDAttributesViewController: TestBaseViewController {
     // 异步+手动启动
     let anotherQueue = DispatchQueue(label: "com.appcoda.anotherQueue", qos: .userInitiated, attributes: [.concurrent, .initiallyInactive])
     
+    var activateTag = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        anotherQueue.async {
-            for i in 0 ..< 10 {
-                JPrint("🍌", i)
-            }
-        }
-        
-        anotherQueue.async {
-            for i in 10 ..< 20 {
-                JPrint("🌶", i)
-            }
-        }
-        
-        anotherQueue.async {
-            for i in 20 ..< 30 {
-                JPrint("🍍", i)
-            }
-        }
+        setupQueue()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         addFunAction { [weak self] in
             guard let self = self else { return }
+            
+            // 手动执行
+            self.anotherQueue.activate()
+            // 可以调用多次执行，但已经执行过的任务只会执行一次，不会重复执行
+            self.anotherQueue.activate()
+            self.anotherQueue.activate()
+            
+            // 添加新的任务可以再次调用`activate`执行
+            self.setupQueue()
+            // 已经执行过的任务只会执行一次，不会重复执行
+            self.anotherQueue.activate()
+            self.anotherQueue.activate()
             self.anotherQueue.activate()
         }
     }
@@ -57,8 +55,30 @@ class GCDAttributesViewController: TestBaseViewController {
     }
     
     deinit {
-        // 一定要保证执行了！否则当持有该队列的对象销毁时会崩溃！
+        // 一定要确保队列已经执行了！否则当持有该队列的对象销毁时会崩溃！
         anotherQueue.activate()
     }
     
+    func setupQueue() {
+        activateTag += 1
+        let thisTag = activateTag
+        
+        anotherQueue.async {
+            for i in 0 ..< 10 {
+                JPrint("tag ---", thisTag, "🍌", i)
+            }
+        }
+        
+        anotherQueue.async {
+            for i in 10 ..< 20 {
+                JPrint("tag ---", thisTag, "🌶", i)
+            }
+        }
+        
+        anotherQueue.async {
+            for i in 20 ..< 30 {
+                JPrint("tag ---", thisTag, "🍍", i)
+            }
+        }
+    }
 }
