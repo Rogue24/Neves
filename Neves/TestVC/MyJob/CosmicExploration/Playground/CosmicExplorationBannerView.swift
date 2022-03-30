dbd7ca89b821f66f0aaa5a9d6ef957e08b1c9e86
zhoujianping <zhoujianping@52tt.com>
2022-03-30 17:27:11 +0800

    喇叭图标

--- Neves/TestVC/MyJob/CosmicExploration/Playground/CosmicExplorationBannerView.swift
+++ Neves/TestVC/MyJob/CosmicExploration/Playground/CosmicExplorationBannerView.swift
@@ -5,13 +5,27 @@
 //  Created by aa on 2022/3/28.
 //
 
+import UIKit
+
 class CosmicExplorationBannerView: UIView {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var leftIconWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var leftIconLeftConstraint: NSLayoutConstraint!
+    @IBOutlet weak var tableViewLeftConstraint: NSLayoutConstraint!
+    @IBOutlet weak var tableViewRightConstraint: NSLayoutConstraint!
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
         layer.cornerRadius = 11.px
         layer.masksToBounds = true
+        
+        leftIconWidthConstraint.constant = 15.px
+        leftIconLeftConstraint.constant = 5.px
+        tableViewLeftConstraint.constant = 5.px
+        tableViewRightConstraint.constant = 5.px
     }
     
 }
