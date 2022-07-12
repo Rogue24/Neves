9b7ff346a5095d68e620bc6efcfc1f670857d348
zhoujianping <zhoujianping@52tt.com>
2022-07-12 16:19:49 +0800

    位置调整

---
+++ Neves/TestVC/Base/Diffable/DiffableTestViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DiffableTestViewController.swift
+//  Neves
+//
+//  Created by aa on 2022/7/12.
+//
+
+import UIKit
+
+class DiffableTestViewController: TestBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
