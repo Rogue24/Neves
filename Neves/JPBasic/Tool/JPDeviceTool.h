ac056ba719d6dde0aa9b8ea3f02b940a74b9aa6d
zhoujianping <zhoujianping@52tt.com>
2022-09-17 23:57:03 +0800

    适配iOS 16的屏幕旋转

--- Neves/JPBasic/Tool/JPDeviceTool.h
+++ Neves/JPBasic/Tool/JPDeviceTool.h
@@ -8,13 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSUInteger, JPDeviceOrientation) {
+    JPDeviceOrientationPortrait,            // Device oriented vertically, home button on the bottom
+    JPDeviceOrientationLandscapeLeft,       // Device oriented horizontally, home button on the right
+    JPDeviceOrientationLandscapeRight,      // Device oriented horizontally, home button on the left
+};
+
 @interface JPDeviceTool : NSObject
 + (NSString *)deviceName;
 + (NSString *)systemVersion;
 
 + (BOOL)isPortrait;
-+ (UIDeviceOrientation)currentOrientation;
-+ (void)switchDeviceOrientation:(UIDeviceOrientation)orientation;
++ (JPDeviceOrientation)currentOrientation;
++ (void)switchDeviceOrientation:(JPDeviceOrientation)orientation;
 
 + (int)getSignalIntensity:(BOOL)isWiFi;
 
--- Neves/JPBasic/Tool/JPDeviceTool.m
+++ Neves/JPBasic/Tool/JPDeviceTool.m
@@ -24,40 +24,92 @@
     return systemVersion_;
 }
 
++ (JPDeviceOrientation)convertUIDeviceOrientation:(UIDeviceOrientation)orientation {
+    switch (orientation) {
+        case UIDeviceOrientationLandscapeLeft:
+            return JPDeviceOrientationLandscapeLeft;
+        case UIDeviceOrientationLandscapeRight:
+            return JPDeviceOrientationLandscapeRight;
+        default:
+            return JPDeviceOrientationPortrait;
+    }
+}
+
++ (JPDeviceOrientation)convertUIInterfaceOrientation:(UIInterfaceOrientation)orientation {
+    switch (orientation) {
+        case UIInterfaceOrientationLandscapeRight:
+            return JPDeviceOrientationLandscapeLeft;
+        case UIInterfaceOrientationLandscapeLeft:
+            return JPDeviceOrientationLandscapeRight;
+        default:
+            return JPDeviceOrientationPortrait;
+    }
+}
+
++ (JPDeviceOrientation)currentOrientation {
+    if (@available(iOS 16.0, *)) {
+        UIWindowScene *scene = (UIWindowScene *)[UIApplication sharedApplication].connectedScenes.allObjects.firstObject;
+        return [self convertUIInterfaceOrientation:scene.interfaceOrientation];
+    }
+    return [self convertUIDeviceOrientation:[UIDevice currentDevice].orientation];
+}
+
 + (BOOL)isPortrait {
-    return self.currentOrientation == UIDeviceOrientationPortrait;
+    return self.currentOrientation == JPDeviceOrientationPortrait;
 }
 
-+ (UIDeviceOrientation)currentOrientation {
-    UIDeviceOrientation orientation = [UIDevice currentDevice].orientation;
-    if (orientation == UIDeviceOrientationUnknown ||
-        orientation == UIDeviceOrientationPortraitUpsideDown ||
-        orientation == UIDeviceOrientationFaceUp ||
-        orientation == UIDeviceOrientationFaceDown) {
-        orientation = UIDeviceOrientationPortrait;
-    }
-    return orientation;
-}
-
-+ (void)switchDeviceOrientation:(UIDeviceOrientation)orientation {
-    if ([UIDevice currentDevice].generatesDeviceOrientationNotifications) {
-        UIDevice *currentDevice = [UIDevice currentDevice];
-        UIDeviceOrientation currentOrientation = currentDevice.orientation;
-        NSString *keyStr = JPKeyPath(currentDevice, orientation);
-        [currentDevice setValue:@(orientation) forKey:keyStr];
-        [UIViewController attemptRotationToDeviceOrientation];
-        if (currentOrientation == orientation) {
-            // 如果方向已经相同了，系统不会发通知，手动发个呗
-            JPPostNotification(UIDeviceOrientationDidChangeNotification, nil, nil);
++ (void)switchDeviceOrientation:(JPDeviceOrientation)orientation {
+    [UIViewController attemptRotationToDeviceOrientation];
+    
+    // 屏幕旋转适配iOS16
+    // 参考1：https://www.jianshu.com/p/ff6ed9de906d
+    // 参考2：https://blog.csdn.net/wujakf/article/details/126133680
+    // `iOS16`之后监听`UIDeviceOrientationDidChangeNotification`通知就不好使了（有时候有，有时候没有），
+    // 重写`UIViewController`的`viewWillTransitionToSize:withTransitionCoordinator:`方法即可监听屏幕的旋转。
+    if (@available(iOS 16.0, *)) {
+        UIInterfaceOrientationMask orientationMark;
+        switch (orientation) {
+            case JPDeviceOrientationLandscapeLeft:
+                orientationMark = UIInterfaceOrientationMaskLandscapeRight;
+                break;
+            case JPDeviceOrientationLandscapeRight:
+                orientationMark = UIInterfaceOrientationMaskLandscapeLeft;
+                break;
+            default:
+                orientationMark = UIInterfaceOrientationMaskPortrait;
+                break;
         }
+        
+        UIWindowSceneGeometryPreferencesIOS *geometryPreferencesIOS = [[UIWindowSceneGeometryPreferencesIOS alloc] initWithInterfaceOrientations:orientationMark];
+        
+        NSArray *connectedScenes = [UIApplication sharedApplication].connectedScenes.allObjects;
+        for (UIScene *scene in connectedScenes) {
+            if ([scene isKindOfClass:UIWindowScene.class]) {
+                // 一般来说app只有一个windowScene，而windowScene内可能有多个window。
+                UIWindowScene *windowScene = (UIWindowScene *)scene;
+                // 例如Neves中至少有两个window：第一个是app主体的window，第二个则是FunnyButton所在的window。
+                for (UIWindow *window in windowScene.windows) {
+                    [window.windowScene requestGeometryUpdateWithPreferences:geometryPreferencesIOS errorHandler:^(NSError * _Nonnull error) {
+                        JPLog(@"强制旋转错误: %@", error);
+                    }];
+                }
+            }
+        }
+        
+        return;
     }
+    
+    UIDevice *currentDevice = [UIDevice currentDevice];
+    NSString *keyStr = JPKeyPath(currentDevice, orientation);
+    [currentDevice setValue:@(orientation) forKey:keyStr];
 }
 
 + (int)getSignalIntensity:(BOOL)isWiFi {
     int signalStrength = 0;
     if (@available(iOS 13.0, *)) {
         id statusBar = nil;
-        UIStatusBarManager *statusBarManager = [UIApplication sharedApplication].keyWindow.windowScene.statusBarManager;
+        // 一般来说第一个window就是app主体的window。
+        UIStatusBarManager *statusBarManager = [UIApplication sharedApplication].windows.firstObject.windowScene.statusBarManager;
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wundeclared-selector"
         if ([statusBarManager respondsToSelector:@selector(createLocalStatusBar)]) {
@@ -117,7 +169,10 @@
         if (@available(iOS 10.0, *)) {
             [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
         } else {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
             [[UIApplication sharedApplication] openURL:url];
+#pragma clang diagnostic pop
         }
         return YES;
     } else {
