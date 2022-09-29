3c420492980548b40f891776a8982e2120fd5ba6
zhoujianping <zhoujianping@52tt.com>
2022-09-29 18:49:25 +0800

    添加注释

--- Neves/JPBasic/Tool/JPScreenRotationTool.h
+++ Neves/JPBasic/Tool/JPScreenRotationTool.h
@@ -9,28 +9,46 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/** 屏幕方向发生改变的通知 */
 UIKIT_EXTERN NSNotificationName const JPScreenOrientationDidChangeNotification;
 
+/** 可旋转的屏幕方向 */
 typedef NS_ENUM(NSUInteger, JPScreenOrientation) {
-    JPScreenOrientationPortrait,            // vertically, home button on the bottom
-    JPScreenOrientationLandscapeLeft,       // horizontally, home button on the right
-    JPScreenOrientationLandscapeRight,      // horizontally, home button on the left
+    JPScreenOrientationPortrait,        // 竖屏 手机头在上边
+    JPScreenOrientationLandscapeLeft,   // 横屏 手机头在左边
+    JPScreenOrientationLandscapeRight,  // 横屏 手机头在右边
 };
 
 @interface JPScreenRotationTool : NSObject
+/** 单例 */
 + (instancetype)sharedInstance;
 
+/** 是否正在竖屏 */
 @property (nonatomic, assign, readonly) BOOL isPortrait;
+/** 当前屏幕方向（JPScreenOrientation） */
 @property (nonatomic, assign, readonly) JPScreenOrientation orientation;
+/** 当前屏幕方向（UIInterfaceOrientationMask）  */
 @property (nonatomic, assign, readonly) UIInterfaceOrientationMask orientationMask;
 
+/** 屏幕方向发生改变的回调 */
 @property (nonatomic, copy) void (^_Nullable orientationMaskDidChange)(UIInterfaceOrientationMask orientationMask);
 
+/** 旋转至目标方向 */
 - (void)rotationToOrientation:(JPScreenOrientation)orientation;
+
+/** 旋转至竖屏 */
 - (void)rotationToPortrait;
+
+/** 旋转至横屏（手机头在左边） */
 - (void)rotationToLandscapeLeft;
+
+/** 旋转至横屏（手机头在右边） */
 - (void)rotationToLandscapeRight;
+
+/** 旋转至横屏（如果锁定了屏幕，则转向手机头在左边） */
 - (void)rotationToLandscape;
+
+/** 横竖屏切换 */
 - (void)toggleOrientation;
 @end
 
