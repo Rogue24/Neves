b05d957f54ceb586d710a9ffc12bcdde4f97c11e
zhoujianping <zhoujianping24@hotmail.com>
2024-10-22 20:56:26 +0800

    添加DeepLabV3调试页

---
+++ Neves/TestVC/Image/DeepLabV3/DeepLabV3ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DeepLabV3ViewController.swift
+//  Neves
+//
+//  Created by aa on 2024/10/22.
+//
+
+import UIKit
+
+class DeepLabV3ViewController: TestBaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
