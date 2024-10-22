84aed7717f8a1ea4414c2d18a1098bf4234a76e7
zhoujianping <zhoujianping24@hotmail.com>
2024-10-22 21:09:44 +0800

    add test code

--- Neves/TestVC/Image/DeepLabV3/DeepLabV3ViewController.swift
+++ Neves/TestVC/Image/DeepLabV3/DeepLabV3ViewController.swift
@@ -6,10 +6,32 @@
 //
 
 import UIKit
+import FunnyButton
+import CoreML
 
 class DeepLabV3ViewController: TestBaseViewController {
+    
+    lazy var model = try! DeepLabV3(configuration: {
+        let config = MLModelConfiguration()
+        config.allowLowPrecisionAccumulationOnGPU = true
+        if #available(iOS 16.0, *) {
+            config.computeUnits = .cpuAndNeuralEngine
+        } else {
+            // Fallback on earlier versions
+        }
+        return config
+    }())
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        removeFunnyActions()
+        addFunnyAction {
+            JPrint("DeepLabV3 test")
+        }
+    }
+    
 }
