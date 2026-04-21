f39800ce85db61c2f3cb318364b6ce30d5f1108b
zhoujianping <zhoujianping24@hotmail.com>
2026-04-21 14:19:17 +0800

    更新工具和扩展方法

--- Neves/Neves/Extension/URL.Extension.swift
+++ Neves/Neves/Extension/URL.Extension.swift
@@ -8,4 +8,26 @@
 extension URL: JPCompatible {}
 extension JP where Base == URL {
     static var hitokoto: Base { Base(string: "https://v1.hitokoto.cn")! }
+    
+    /// 安全获取路径，支持选择是否保留百分号编码（兼容旧版本）
+    /// 🌰🌰🌰 `"file:///Users/zhoujianping/My%20Documents/file.txt"`
+    /// `percentEncoded: false => /Users/zhoujianping/My Documents/file.txt`
+    /// `percentEncoded: true => /Users/zhoujianping/My%20Documents/file.txt`
+    func safePath(percentEncoded: Bool = true) -> String {
+        if #available(iOS 16.0, macOS 13.0, *) {
+            return base.path(percentEncoded: percentEncoded)
+        }
+        
+        // iOS 15 及以下用 URLComponents 实现
+        guard percentEncoded, let components = URLComponents(url: base, resolvingAgainstBaseURL: false) else {
+            return base.path
+        }
+        
+        return components.percentEncodedPath
+    }
+    
+    /// 该路径是否为文件夹（不存在也会返回 false）
+    var isDirectory: Bool {
+        (try? base.resourceValues(forKeys: [.isDirectoryKey]).isDirectory) ?? false
+    }
 }
